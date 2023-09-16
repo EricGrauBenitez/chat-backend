e616b7e3cce96f35d04bfef78c67b195abd7fb6a
EricGrauBenitez <112932407+EricGrauBenitez@users.noreply.github.com>
2023-09-17 00:26:06 +0200

    Add files via upload

---
+++ routes/chat.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const chatController = require('../controllers/chatController');
+
+// Ruta para guardar el chat
+router.post('/:userId', chatController.saveChat);
+
+router.get('/:userId', chatController.getUserChats);
+
+router.get('/:userId/:chatId', chatController.getConversationByChatId);
+
+router.delete('/:userId/:chatId', chatController.deleteChat);
+
+router.put('/conversation/:userId/:chatId', chatController.cleanConversation);
+
+router.put('/:userId/:chatId', chatController.updateChat)
+
+
+module.exports = router;
